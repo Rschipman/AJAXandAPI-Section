7421d7ef53c0ecc30904d58f3014c41930657d55
RChipman <rschipman@gmail.com>
2023-02-19 18:28:31 -0300

    thats so fetch

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const req = new XMLHttpRequest();
+// const req = new XMLHttpRequest();
 
-req.onload = function () {
-  console.log("It Loaded!");
-  const data = JSON.parse(this.responseText);
-  console.log(`${data.name}, ${data.height}cm`);
-};
+// req.onload = function () {
+//   console.log("It Loaded!");
+//   const data = JSON.parse(this.responseText);
+//   console.log(`${data.name}, ${data.height}cm`);
+// };
 
-req.onerror = function () {
-  console.log("ERROR!!");
-  console.log(this);
-};
-req.open("GET", "https://swapi.dev/api/people/1/");
-req.send();
+// req.onerror = function () {
+//   console.log("ERROR!!");
+//   console.log(this);
+// };
+// req.open("GET", "https://swapi.dev/api/people/1/");
+// req.send();
 
 // "https://swapi.dev/api/people/1/"
+
+fetch("https://swapi.dev/api/people/1/")
+  .then((res) => {
+    console.log("Resolved", res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log("Error!", e);
+  });
