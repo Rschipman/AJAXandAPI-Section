50b3c7882693bf474266fc73b729bdb9b718d227
RChipman <rschipman@gmail.com>
2023-02-19 18:37:38 -0300

    stop trying to make fetch happen

--- script.js
+++ script.js
@@ -15,14 +15,29 @@
 
 // "https://swapi.dev/api/people/1/"
 
-fetch("https://swapi.dev/api/people/1/")
-  .then((res) => {
-    console.log("Resolved", res);
-    return res.json();
-  })
-  .then((data) => {
+// fetch("https://swapi.dev/api/people/1/")
+//   .then((res) => {
+//     console.log("Resolved", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((e) => {
+//     console.log("Error!", e);
+//   });
+
+const loadStarWarsPeople = async () => {
+  try {
+    const res = await fetch("https://swapi.dev/api/people/1/");
+    const data = await res.json();
     console.log(data);
-  })
-  .catch((e) => {
-    console.log("Error!", e);
-  });
+    const res2 = await fetch("https://swapi.dev/api/people/2/");
+    const data2 = await res2.json();
+    console.log(data2);
+  } catch (e) {
+    console.log("ERROR!!", e);
+  }
+};
+
+loadStarWarsPeople();
