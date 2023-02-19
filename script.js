b52a2490a7b26af14b57284dddb6c109a5a2dfef
RChipman <rschipman@gmail.com>
2023-02-19 18:44:33 -0300

    axios

--- script.js
+++ script.js
@@ -27,17 +27,21 @@
 //     console.log("Error!", e);
 //   });
 
-const loadStarWarsPeople = async () => {
-  try {
-    const res = await fetch("https://swapi.dev/api/people/1/");
-    const data = await res.json();
-    console.log(data);
-    const res2 = await fetch("https://swapi.dev/api/people/2/");
-    const data2 = await res2.json();
-    console.log(data2);
-  } catch (e) {
-    console.log("ERROR!!", e);
-  }
-};
+// const loadStarWarsPeople = async () => {
+//   try {
+//     const res = await fetch("https://swapi.dev/api/people/1/");
+//     const data = await res.json();
+//     console.log(data);
+//     const res2 = await fetch("https://swapi.dev/api/people/2/");
+//     const data2 = await res2.json();
+//     console.log(data2);
+//   } catch (e) {
+//     console.log("ERROR!!", e);
+//   }
+// };
 
-loadStarWarsPeople();
+// loadStarWarsPeople();
+
+axios.get("https://swapi.dev/api/people/1/").then((res) => {
+  console.log("Resolved", res);
+});
