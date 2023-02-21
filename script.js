bf802dd281739bf81067293426fc5ec9a07a1ca9
RChipman <rschipman@gmail.com>
2023-02-20 22:44:32 -0300

    stuff

--- script.js
+++ script.js
@@ -62,8 +62,26 @@
 
 // getStarWarsPerson(4);
 
-const getDadJoke = async () => {
-  const config = { headers: { Accept: "application/json" } };
-  const res = await axios.get("https://icanhazdadjoke.com/", config);
-  console.log(res);
+const jokes = document.querySelector("#jokes");
+
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  //   console.log(jokeText);
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
 };
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "No jokes available, sorry :(";
+  }
+};
+
+button.addEventListener("click", addNewJoke);
